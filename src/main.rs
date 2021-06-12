7107eb04757a8652f1cb1b93e46ed56bdf0e4aac
Linas Nikiperavičius <linas@linasdev.com>
2021-06-12 15:09:06 +0300

    Implement simple bootloader

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,19 @@
 
 use panic_semihosting as _;
 
-use cortex_m::asm;
+use cortex_m::asm::bootload;
 use cortex_m_rt::entry;
+use stm32f1xx_hal::prelude::*;
+
+static PROGRAM_ADDRESS: u32 = 0x0800_4000;
 
 #[entry]
 fn main() -> ! {
-    loop {
-        asm::nop();
+    boot();
+}
+
+fn boot() -> ! {
+    unsafe {
+        bootload(PROGRAM_ADDRESS as *const u32);
     }
 }
