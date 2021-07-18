f24fbe0bf23aa345b0acc83d950d6f436ab27630
Linas Nikiperavičius <linas@linasdev.com>
2021-07-18 12:59:29 +0300

    Change flash script

--- flash.sh
+++ flash.sh
@@ -1,3 +1,11 @@
-#!/bin/sh
-cargo objcopy --release -- -O binary ./target/thumbv7m-none-eabi/release/ross-bootloader.bin
-openocd -f interface/stlink.cfg -f target/stm32f1x.cfg -c "init; reset halt; flash erase_address 0x08000000 0x00008000; flash write_bank 0 ./target/thumbv7m-none-eabi/release/ross-bootloader.bin 0x00000000; reset run; shutdown;"
+#!/bin/bash
+cargo build --release
+objcopy -I elf32-little -O binary ./target/thumbv7m-none-eabi/release/ross-bootloader ./target/thumbv7m-none-eabi/release/ross-bootloader.bin
+openocd -f ./openocd.cfg                                                                        \
+    -c "init;"                                                                                  \
+    -c "reset halt;"                                                                            \
+    -c "flash erase_address 0x08000000 0x00008000;"                                             \
+    -c "flash write_bank 0 ./target/thumbv7m-none-eabi/release/ross-bootloader.bin 0x00000000;" \
+    -c "tpiu config external uart off 72000000 2000000"                                         \
+    -c "itm port 0 on"                                                                          \
+    -c "reset run;"
